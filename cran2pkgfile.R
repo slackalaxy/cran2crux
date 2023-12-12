310c98cce4f6856e91d92e0c932b48bad6616a21
Petar B. Petrov <slackalaxy@gmail.com>
2023-12-12 23:25:47 +0200

    Add files via upload

---
+++ cran2pkgfile.R
@@ -0,0 +1,183 @@
+#!/usr/bin/env Rscript
+
+# Load the available packages database
+#module <- "Seurat"
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+args <- commandArgs(trailingOnly = TRUE)
+module <- args[1]
+cliopt <- args[2]
+depth <- args[3]
+
+pkgsdb <- available.packages()
+
+# is a package on CRAN?
+on.cran <- function(x) {
+  result <- try(pkgsdb[x, "Package"], silent = T)
+  if (inherits(result, 'try-error')) {
+    cat(x, "is not on CRAN. Skipping...", "\n")
+    return(NULL)
+  }
+  return(result)
+}
+
+# stop already if input does not exist
+if (is.null(on.cran(module))) {
+  cat("Input is not on CRAN. Stopping... ")
+  stop()
+}
+
+# leave only deps available on CRAN
+cran.available <- function(x){
+  y <- c()
+  for (i in x) {
+    j <- on.cran(i)
+    y <- c(y, j)
+  }
+  return(y)
+}
+
+# remove anything with or within brackets
+names.only <- function(x){
+  x <- gsub("[\r\n]", " ", x)
+  x <- strsplit(x, ", ")
+
+  y <- c()
+  for (i in x[[1]]) {
+    j <- gsub("\\s*\\([^\\)]+\\)","",as.character(i))
+    y <- c(y, j)
+  }
+  
+  return(y)
+}
+
+# Depends on
+depends.on <- function(package = NULL,
+                       pkgs.db = NULL){
+  
+  depends <- names.only(pkgs.db[package, "Depends"])
+  imports <- names.only(pkgs.db[package, "Imports"])
+  linking <- names.only(pkgs.db[package, "LinkingTo"])
+  
+  dependencies <- c(depends, imports, linking)
+  y <- sort(unique(cran.available(dependencies)))
+  
+  return(y)
+}
+
+# Optional
+optional <- function(package = NULL,
+                     pkgs.db = NULL){
+
+  optional <- names.only(pkgs.db[package, "Suggests"])
+  y <- sort(cran.available(optional))
+  
+  return(y)
+}
+
+# This is able to retrieve deps of multiple packages
+depsofdeps <- function(modules = NULL, pkgsdb = NULL, opts = NULL){
+  deep <- c(modules)
+  for (i in deep) {
+    d <- depends.on(i, pkgsdb)
+    if (isTRUE(opts)) {
+      o <- optional(i, pkgsdb)
+      d <- unique(c(d, o))
+    }
+    deep <- c(deep, d)
+    deep <- sort(unique(deep))
+  }
+  return(deep)
+}
+
+# This runs depsofdeps recursively with a number of iterations, deep resolving deps
+deepdeps <- function(package = NULL,
+                     pkgs.db = NULL,
+                     iterations = 3,
+                     opts = NULL){
+  
+  # start with just the one
+  deep <- depsofdeps(package, pkgsdb)
+  
+  for (n in 1:iterations) {
+    deep <- depsofdeps(deep, pkgsdb = pkgsdb, opts)
+    nam <- paste0("deep.", n)
+    assign(nam, deep)
+    deep <- depsofdeps(deep, pkgsdb = pkgsdb, opts)
+  }
+  return(deep)
+}
+
+# prepare for "Depends on" or "Optional" rows in the Pkgfile
+pkgfile.style <- function(x){
+  y <- c()
+  for (i in x) {
+    j <- paste0("r4-", tolower(i))
+    y <- c(y, j)
+  }
+  return(y)
+}
+
+# Write the Pkgfile
+pkgfile.write <- function(module = NULL){
+  
+  # TODO: this should be set elsewhere
+  maintainer <- c("Petar Petrov, slackalaxy at gmail dot com")
+  
+  # Preserve upper case, as well as original version format of the module
+  modules.dep <- depends.on(module, pkgsdb)
+  modules.opt <- optional(module, pkgsdb)
+  modules.ver <- pkgsdb[module, "Version"]
+  
+  # polish for Pkgfile's fields
+  pkgfile.dsc <- paste("R module", module)
+  pkgfile.url <- paste0("https://cran.r-project.org/web/packages/", module)
+  pkgfile.mnt <- maintainer
+  pkgfile.dep <- gsub(",", "", gsub("\\.", "-", toString(pkgfile.style(modules.dep))))
+  pkgfile.opt <- gsub(",", "", gsub("\\.", "-", toString(pkgfile.style(modules.opt))))
+  pkgfile.nam <- paste0("r4-", tolower(gsub("\\.", "-", module)))
+  pkgfile.ver <- gsub("-", "\\.", modules.ver)
+  
+  if (modules.ver == pkgfile.ver) {
+    modules.ver <- "${version}"
+  }
+  
+  pkgfile.rel <- "1"
+  pkgfile.src <- paste0(available.packages()[module, "Repository"], "/", module, "_", modules.ver, ".tar.gz")
+  
+  pkgfile <- paste0("# Description: ", pkgfile.dsc, "\n",
+                    "# URL: ", pkgfile.url, "\n",
+                    "# Maintainer: ", pkgfile.mnt, "\n",
+                    "# Depends on: r ", pkgfile.dep, "\n",
+                    "# Optional: ", pkgfile.opt, "\n",
+                    "\n",
+                    "name=", pkgfile.nam, "\n",
+                    "version=", pkgfile.ver, "\n",
+                    "release=", pkgfile.rel, "\n",
+                    "source=", "(", pkgfile.src, ")", "\n",
+                    "\n",
+                    "build() {", "\n",
+                    "\t", "cd ", module, "\n",
+                    "\t", "mkdir -p $PKG/usr/lib/R/library", "\n",
+                    "\t", "R CMD INSTALL . -l $PKG/usr/lib/R/library", "\n",
+                    "}")
+  
+  dir.create(pkgfile.nam)
+  write(pkgfile, paste0(pkgfile.nam, "/", "Pkgfile"))
+  cat("=======> Created", "port for", module, ":", paste0(pkgfile.nam, "/", "Pkgfile"), "\n")
+  #return(pkgfile)
+}
+
+if (cliopt == "-r" | cliopt == "--recursive") {
+  modules.all <- deepdeps(module, pkgsdb, depth, opts = FALSE)
+  for (d in modules.all){
+    pkgfile.write(d)
+  }
+}else if(cliopt == "-ro" | cliopt == "--recursive-opt") {
+  modules.all <- deepdeps(module, pkgsdb, depth, opts = TRUE)
+  for (d in modules.all){
+    pkgfile.write(d)
+ }
+}else{
+  pkgfile.write(module)
+}
