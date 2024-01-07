4b9d100d34d397dce3781af27c81d01935d68c2f
Petar Petrov <slackalaxy@gmail.com>
2024-01-07 12:00:56 +0200

    modified:   old2new.R

--- old2new.R
+++ old2new.R
@@ -8,14 +8,19 @@ options(repos = c(CRAN = cranrepo.url))
 
 # Report modules with potential updates
 show.old <- function(){
-  old <- as.data.frame(old.packages())
-  #row.names(old) <- NULL
-  display <- data.frame(Module = old$Package,
-                        Installed = old$Installed,
-                        ReposVer = old$ReposVer,
-                        Port = paste0("r4-", tolower(gsub("\\.", "-", old$Package))))
+  if (!is.null(old.packages())) {
+    old <- as.data.frame(old.packages())
+    #row.names(old) <- NULL
+    display <- data.frame(Module = old$Package,
+                          Installed = old$Installed,
+                          ReposVer = old$ReposVer,
+                          Port = paste0("r4-", tolower(gsub("\\.", "-", old$Package))))
+    
+    return(display)
+  }else{
+    return(paste("No differences found."))
+  }
   
-  return(display)
 }
 
 # show old
