444468d2833656378b40c1aed8c60e9077266075
Petar Petrov <slackalaxy@gmail.com>
2025-11-15 21:39:56 +0200

    modified:   old2new.R

--- old2new.R
+++ old2new.R
@@ -8,18 +8,17 @@ options(repos = c(CRAN = cranrepo.url))
 
 # Report modules with potential updates
 show.old <- function(){
-  if (!is.null(old.packages())) {
     old <- as.data.frame(suppressMessages(old.packages(repos = BiocManager::repositories())))
-    display <- data.frame(Port = paste0("r4-", tolower(gsub("\\.", "-", old$Package))),
-                          Module = old$Package,
-                          Installed = old$Installed,
-                          ReposVer = old$ReposVer)
-    
-    return(print(display, row.names = F))
-  }else{
-    return(cat("All packages are up to date.\n"))
-  }
-  
+    if (nrow(old) > 0) {
+      display <- data.frame(Port = paste0("r4-", tolower(gsub("\\.", "-", old$Package))),
+                            Module = old$Package,
+                            Installed = old$Installed,
+                            ReposVer = old$ReposVer)
+      
+      return(print(display, row.names = F))
+    } else {
+      return(cat("All packages are up to date.\n"))
+    }
 }
 
 show.old()
