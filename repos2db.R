074411eed61939a986f2e0e8515ec95841ed928f
Petar Petrov <slackalaxy@gmail.com>
2026-05-02 22:41:17 +0300

    modified:   repos2db.R

--- repos2db.R
+++ repos2db.R
@@ -2,5 +2,7 @@
 
 # get cranrepo and maintainer information
 source("/etc/cran2crux.conf")
+cat("... Downloading libraries database", "\n")
 pkgsdb <- suppressMessages(available.packages(repos = BiocManager::repositories()))
 saveRDS(pkgsdb, "/tmp/pkgsdb.rds")
+cat("... Done! Saved as /tmp/pkgsdb.rds", "\n")
