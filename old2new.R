1813ac97b4917db6ec9ecc1bb508324cf25317ed
Petar Petrov <slackalaxy@gmail.com>
2026-05-03 12:07:56 +0300

    modified:   old2new.R

--- old2new.R
+++ old2new.R
@@ -3,13 +3,19 @@
 # get cranrepo and maintainer information
 source("/etc/cran2crux.conf")
 
+# get RDS path exported from the cran2crux bash script
+rds_path <- Sys.getenv("RDS_PATH")
+
 # set cran repo
-options(repos = c(CRAN = cranrepo.url))
+#options(repos = c(CRAN = cranrepo.url))
+
+# This must be generated in advance by repos2db.R
+old <- readRDS(paste0(rds_path, "old.rds"))
 
 # Report modules with potential updates
 show.old <- function(){
   cat("... Checking for updates of installed packages", "\n")
-  old <- as.data.frame(suppressMessages(old.packages(repos = BiocManager::repositories())))
+  #old <- as.data.frame(suppressMessages(old.packages(repos = BiocManager::repositories())))
   if (nrow(old) > 0) {
     display <- data.frame(Port = paste0("r4-", tolower(gsub("\\.", "-", old$Package))),
                           Module = old$Package,
