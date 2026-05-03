13651b5ca722360ee6a4ab4da0e739b6a319b5db
Petar Petrov <slackalaxy@gmail.com>
2026-05-03 12:08:06 +0300

    modified:   repos2db.R

--- repos2db.R
+++ repos2db.R
@@ -2,7 +2,21 @@
 
 # get cranrepo and maintainer information
 source("/etc/cran2crux.conf")
+
+# get RDS path exported from the cran2crux bash script
+rds_path <- Sys.getenv("RDS_PATH")
+
+# set cran repo
+options(repos = c(CRAN = cranrepo.url))
+
 cat("... Downloading libraries database", "\n")
+
+# check for old
+old <- as.data.frame(suppressMessages(old.packages(repos = BiocManager::repositories())))
+saveRDS(pkgsdb, paste0(rds_path, "/tmp/old.rds"))
+
+# download all available
 pkgsdb <- suppressMessages(available.packages(repos = BiocManager::repositories()))
-saveRDS(pkgsdb, "/tmp/pkgsdb.rds")
+saveRDS(pkgsdb, paste0(rds_path, "/tmp/pkgsdb.rds"))
+
 cat("... Done! Saved as /tmp/pkgsdb.rds", "\n")
