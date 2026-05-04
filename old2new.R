52abbf90bd5c8e1fdcc63232c59227ddf1cd1808
Petar Petrov <slackalaxy@gmail.com>
2026-05-04 21:30:04 +0300

    modified:   old2new.R

--- old2new.R
+++ old2new.R
@@ -13,6 +13,21 @@ rds_path <- args[1]
 # This must be generated in advance by repos2db.R
 old <- readRDS(paste0(rds_path, "old.rds"))
 
+# Upstream repo better name
+upstream <- function(up = NULL){
+  
+  upstream <- unlist(strsplit(up, "\\/"))[3]
+  
+  if(upstream == "cloud.r-project.org"){
+    u <- "CRAN"
+  }else if(upstream == "bioconductor.org"){
+    u <- "BioC"
+  } else {
+    u <- "UNKNOWN"
+  }
+  return(u)
+}
+
 # Report modules with potential updates
 show.old <- function(){
   #cat("... Checking for updates of installed packages", "\n")
@@ -21,7 +36,8 @@ show.old <- function(){
     display <- data.frame(Port = paste0("r4-", tolower(gsub("\\.", "-", old$Package))),
                           Module = old$Package,
                           Installed = old$Installed,
-                          ReposVer = old$ReposVer)
+                          ReposVer = old$ReposVer,
+                          Repositoty = upstream(old$Repository))
     
     return(print(display, row.names = F))
   } else {
